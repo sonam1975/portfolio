08d56d2e861d17bb49807c35555647fe4477cf17
sonam1975 <56772925+sonam1975@users.noreply.github.com>
2020-06-14 23:01:16 -0700

    Added Procfile to function with HerokuApp

--- server.js
+++ server.js
@@ -7,4 +7,6 @@ app.get("/", (req, res) => {
   res.send("An alligator approaches!");
 });
 
-app.listen(3000, () => console.log("Gator app listening on port 3000!"));
+app.listen(3000 || process.env.PORT, () =>
+  console.log("Gator app listening on port 3000!")
+);
